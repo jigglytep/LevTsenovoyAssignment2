2b46b7a301d5ab4d230df4f2de13ce31fee36330
Lev Tsenovoy <lev.tsenovoy@ibm.com>
2022-09-20 00:27:52 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// 1
+var myNumber;
+// 2
+var myNumber = 5;
+// 3
+var myOtherNumber = 5;
+// 4
+var myTotal = myNumber * myOtherNumber;
+// 5
+
+var myName = "Lev Tsenovoy";
+var myCombo = "";
+var myCombo = myNumber + myOtherNumber;
+// 6
+var myOtherCombo = 0;
+myOtherCombo = myNumber + myOtherNumber;
+// 7
+var myRemainder = 0;
+var myRemainder = myNumber % 5;
+// 8
+var i = 2;
+// 9
+var int = 4;
+// 10
+var myBoolean = 0;
+Boolean(myBoolean);
